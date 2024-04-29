4723a16073ef1a06bf045091122110af30c685a1
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-04-29 16:40:56 +0500

    changes

--- controller/paypal.controller.js
+++ controller/paypal.controller.js
@@ -7,45 +7,16 @@ paypal.configure({
   client_secret: process.env.paypal_secret_key,
 });
 
-// const paypalCheckout = async (req, res) => {
-//   const paymentData = {
-//     intent: "sale",
-//     payer: {
-//       payment_method: "paypal",
-//     },
-//     redirect_urls: {
-//       return_url: "http://localhost:3000/success",
-//       cancel_url: "http://localhost:3000/cancel",
-//     },
-//     transactions: [
-//       {
-//         amount: {
-//           total: "10.00",
-//           currency: "USD",
-//         },
-//         description: "Description of the transaction",
-//       },
-//     ],
-//   };
-
-//   paypal.payment.create(paymentData, (error, payment) => {
-//     if (error) {
-//       console.error(error);
-//       return res.status(500).send("Failed to create payment.");
-//     } else {
-//       // Redirect to PayPal for approval
-//       for (let link of payment.links) {
-//         if (link.rel === "approval_url") {
-//           return res.redirect(link.href);
-//         }
-//       }
-//       return res.status(500).send("No approval_url found in response.");
-//     }
-//   });
-// };
-
 const productPageRender = async (req, res) => {
   res.render("product.pug");
+};
+
+const cancelPayment = async (req, res) => {
+  return res.status(400).json({ message: "the payment was cancelled!" });
+};
+
+const successPayment = async (req, res) => {
+  return res.status(200).json({ message: "payment is made successfully!" });
 };
 
 const paypalCheckout = async (req, res) => {
@@ -104,17 +75,9 @@ const paypalCheckout = async (req, res) => {
   }
 };
 
-const cancelPayment = async (req, res) => {
-  return res.status(400).json({ message: "the payment was cancelled!" });
-};
-
-const successPayment = async (req,res)=>{
-    return res.status(200).json({message: "payment is made successfully!"})
-}
-
 module.exports = {
   paypalCheckout,
   productPageRender,
-  cancelPayment, 
-  successPayment
+  cancelPayment,
+  successPayment,
 };
